d363dcd0077cfdc3399249e3a4876e9f93e55ec7
Дмитрий Митин <DVMitin@stud.kpfu.ru>
2021-12-08 12:05:48 +0300

    Добавьте файлы проекта.

---
+++ DZ_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int j = 3;
+            int i1 = 1;
+            int z = 1;
+            int x = 3;
+            int c = 5;
+            int c1 = 1;
+
+            while (j==3)
+            {
+                if (i1 != 3)
+                {
+                    Console.Write(' ');
+                    i1++;
+                }
+                else
+                {
+                    Console.Write('*');
+                    j--;
+                    Console.WriteLine();
+                }
+            }
+
+            while(j==2)
+            {
+                if (z == 1)
+                {
+                    Console.Write(' ');
+                    z++;
+                }
+                else
+                {
+                    while(z<5)
+                    {
+                        Console.Write('*');
+                        z++;
+                        j--;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            while (x == 3)
+            {
+                if (c1 <= c)
+                {
+                    Console.Write('*');
+                    c1++;
+                }
+            }
+        }
+    }
+}
